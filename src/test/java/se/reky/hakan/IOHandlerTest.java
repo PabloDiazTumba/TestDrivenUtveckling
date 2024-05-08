83c0d1c12f4de52737d76eb6f3cf739428be5182
PabloDiazTumba <pablodiaz147@gmail.com>
2024-05-08 09:59:17 +0200

    TestIO

--- src/test/java/se/reky/hakan/IOHandlerTest.java
+++ src/test/java/se/reky/hakan/IOHandlerTest.java
@@ -1,4 +1,25 @@
 package se.reky.hakan;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class IOHandlerTest {
+
+    @Test
+    public void testNextLine() {
+
+        String input = "Hello, world!";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Scanner scanner = new Scanner(System.in);
+        IOHandler ioHandler = new IOHandler(scanner);
+
+        String result = ioHandler.nextLine();
+
+        assertEquals("Hello, world!", result);
+    }
 }
