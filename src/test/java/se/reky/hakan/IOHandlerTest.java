933dff088fb13f32a824da5df3187ef9d558092b
PabloDiazTumba <pablodiaz147@gmail.com>
2024-05-16 09:18:19 +0200

    PrivatMethodIOTest

--- src/test/java/se/reky/hakan/IOHandlerTest.java
+++ src/test/java/se/reky/hakan/IOHandlerTest.java
@@ -1,41 +1,45 @@
 package se.reky.hakan;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Scanner;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IOHandlerTest {
 
+    private String nextLine(Scanner scanner) {
+        return scanner.nextLine();
+    }
+
+    private boolean hasNextInt(Scanner scanner) {
+        return scanner.hasNextInt();
+    }
+
     @Test
     public void testNextLine() {
-
         String input = "Yes/No";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
         Scanner scanner = new Scanner(System.in);
-        IOHandler ioHandler = new IOHandler(scanner);
 
-        String result = ioHandler.nextLine();
+        String result = nextLine(scanner);
 
         assertEquals("Yes/No", result);
     }
 
     @Test
     public void testHasNextInt() {
-
         String input = "3";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
         Scanner scanner = new Scanner(System.in);
 
-        IOHandler ioHandler = new IOHandler(scanner);
-
-        boolean result = ioHandler.hasNextInt();
+        boolean result = hasNextInt(scanner);
 
         assertTrue(result);
     }
